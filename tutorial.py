857205745694ec0400d8a8193bf6a4789b42a367
Iryna Abrosimova <abrosimovairyna@gmail.com>
2025-08-11 22:22:04 +0200

    add bg

--- tutorial.py
+++ tutorial.py
@@ -9,12 +9,29 @@ pygame.init() # initialize pygame
 
 pygame.display.set_caption('Platformer') # set the current window caption
 
-BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1000, 800 
 FPS = 60
 PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT)) # create window
+
+def get_bg(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect() # give x, y, width and height
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+    
+    return tiles, image
+
+def draw(window, bg, bg_img):
+    for tile in bg:
+        window.blit(bg_img, tile) #draw one image onto another
+
+    pygame.display.update() #Update portions of the screen for software displays
 
 def main(window):
     clock = pygame.time.Clock() #create an object to help track time
@@ -22,11 +39,14 @@ def main(window):
     run = True
     while run:
         clock.tick(FPS) # run 60 frames per second
+        bg, bg_image = get_bg("Blue.png")
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw(window, bg, bg_image)
 
     pygame.quit()
     quit()
