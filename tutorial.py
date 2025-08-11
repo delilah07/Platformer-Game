2cadaffe90f9b0e38c487883dce0054b876d1bf9
Iryna Abrosimova <abrosimovairyna@gmail.com>
2025-08-11 22:39:49 +0200

    add player to the window

--- tutorial.py
+++ tutorial.py
@@ -27,26 +27,61 @@ def get_bg(name):
     
     return tiles, image
 
-def draw(window, bg, bg_img):
+class Player(pygame.sprite.Sprite): # the base class for visible game objects
+    COLOR = (255, 0, 0)
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = 'left'
+        self.animation_count = 0
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != 'left':
+            self.direction = 'left'
+            self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != 'right':
+            self.direction = 'right'
+            self.animation_count = 0
+
+    def loop(self, fps):
+        self.move(self.x_vel, self.y_vel)
+
+    def draw(self, window):
+        pygame.draw.rect(window, self.COLOR, self.rect)
+
+def draw(window, bg, bg_img, player):
     for tile in bg:
         window.blit(bg_img, tile) #draw one image onto another
+
+    player.draw(window)
 
     pygame.display.update() #Update portions of the screen for software displays
 
 def main(window):
     clock = pygame.time.Clock() #create an object to help track time
+    bg, bg_image = get_bg("Blue.png")
+    player = Player(100, 100, 50, 50)
 
     run = True
     while run:
         clock.tick(FPS) # run 60 frames per second
-        bg, bg_image = get_bg("Blue.png")
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
 
-        draw(window, bg, bg_image)
+        draw(window, bg, bg_image, player)
 
     pygame.quit()
     quit()
