322678185849adbc80a27c73d2c2b4ca2826a580
Iryna Abrosimova <abrosimovairyna@gmail.com>
2025-08-12 21:50:33 +0200

    add loading of sprite sheets

--- tutorial.py
+++ tutorial.py
@@ -15,6 +15,34 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT)) # create window
 
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]  # pygame module to transform surfaces - flip vertically and horizontally
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join('assets', dir1, dir2)
+    images = [ f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {    }
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+
+    return all_sprites
+
 def get_bg(name):
     image = pygame.image.load(join("assets", "Background", name))
     _, _, width, height = image.get_rect() # give x, y, width and height
@@ -30,6 +58,7 @@ def get_bg(name):
 class Player(pygame.sprite.Sprite): # the base class for visible game objects
     COLOR = (255, 0, 0)
     GRAVITY = 1
+    SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)
 
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
@@ -65,7 +94,9 @@ class Player(pygame.sprite.Sprite): # the base class for visible game objects
         self.fall_count += 1
 
     def draw(self, window):
-        pygame.draw.rect(window, self.COLOR, self.rect)
+        # pygame.draw.rect(window, self.COLOR, self.rect)
+        self.sprite = self.SPRITES["idle_right"][0]
+        window.blit(self.sprite, (self.rect.x, self.rect.y))
 
 def draw(window, bg, bg_img, player):
     for tile in bg:
